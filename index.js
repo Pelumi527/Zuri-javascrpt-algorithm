6fe4c4f762d85a4c9f871790baebec4d6075bb45
Pelumi <fatolupelumi@gmail.com>
2021-05-21 07:28:34 +0100

    first commit

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-// function convertFahrToCelsius(f){
-//     if ( Number(f)) {      // if it it is a number run the function 
-//         let C = (f -32)*5/9
-//         console.log(C.toFixed(4))  // format to 4 decimal place
-//     } else if (Array.isArray(f)){   
-//         console.log(JSON.stringify(f),"is not a valid number but a/an array.") // if it is an array output this
-//     } else if(typeof(f) === "object"){
-//         console.log(JSON.stringify(f),"is not a valid number but a/an object.") // if it is an object output this
-//     } else if (typeof (f) === "string"){
-//         console.log(JSON.stringify(f),"is not a valid number but a string.") // if it is an string output this
-//     }
-//     else{
-//         console.log(JSON.stringify(f), "undefined")
-//     }
-// };
+ function convertFahrToCelsius(f){
+     if ( Number(f)) {      // if it it is a number run the function 
+        let C = (f -32)*5/9
+        console.log(C.toFixed(4))  // format to 4 decimal place
+    } else if (Array.isArray(f)){   
+        console.log(JSON.stringify(f),"is not a valid number but a/an array.") // if it is an array output this
+   } else if(typeof(f) === "object"){
+       console.log(JSON.stringify(f),"is not a valid number but a/an object.") // if it is an object output this
+    } else if (typeof (f) === "string"){
+        console.log(JSON.stringify(f),"is not a valid number but a string.") // if it is an string output this
+    }     else{
+         console.log(JSON.stringify(f), "undefined")
+    }
+};
 
-// convertFahrToCelsius();
+convertFahrToCelsius();
 
 
 
 
 
 function checkYuGiOh(n){
-    let arr = [];
-        if(typeof(n) === "string"){
-            console.log("invalid parameter:", JSON.stringify(n))
+    let arr = []; // creating an empty array
+        if(typeof(n) === "string"){ 
+            console.log("invalid parameter:", JSON.stringify(n)) // if the input is a string output this
         }else {
-            for (let i=1; i<=Number(n); i++){
-                if(i%2==0 && i%3==0 && i%5==0){
+            for (let i=1; i<=Number(n); i++){ // creating a loop to create an array
+                if(i%2==0 && i%3==0 && i%5==0){ 
                     let i = "yu-gi-oh"
-                    arr.push(i)
-                }else if(i%2==0 && i%3==0){
+                    arr.push(i)                
+                }else if(i%2==0 && i%3==0){    
                     let i = "yu-gi"
                     arr.push(i)
                 }else if(i%2==0 && i%5==0){
@@ -50,9 +49,9 @@ function checkYuGiOh(n){
                 }
                 else{
                     arr.push(i)
-                };
-            }
-            console.log(arr)
+                };                          // checking the factors of i and putting them into an array
+            }       
+            console.log(arr)           
         }
 }
 
